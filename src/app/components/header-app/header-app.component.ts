443f700599d5c382f138693b629a4d4a024572f7
Sergio Roman <sergio.romam@outlook.com>
2024-11-10 16:46:18 -0600

    Responsive menu

--- src/app/components/header-app/header-app.component.ts
+++ src/app/components/header-app/header-app.component.ts
@@ -1,19 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { ScrollService } from '../../services/scroll.service';
 import { AppService } from '../../services/app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-app.component.html',
   styleUrl: './header-app.component.css'
 })
 export class HeaderAppComponent {
+  isMenuOpen = false;
+
   appService = inject(AppService);
   constructor(private scrollService: ScrollService) { }
 
   onNavigateToSection(section: string): void {
     this.scrollService.scrollToElement(section);
+  }
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
