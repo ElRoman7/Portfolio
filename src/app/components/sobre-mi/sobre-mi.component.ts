2a1afcec8d03597342d8e712e8f4dd6c21e875b2
Sergio Roman <sergio.romam@outlook.com>
2024-11-10 11:48:18 -0600

    cv download

--- src/app/components/sobre-mi/sobre-mi.component.ts
+++ src/app/components/sobre-mi/sobre-mi.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sobre-mi',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './sobre-mi.component.html',
-  styleUrl: './sobre-mi.component.css'
+  styleUrls: ['./sobre-mi.component.css']
 })
 export class SobreMiComponent {
-  images : string[] = [
+  images: string[] = [
     'assets/img/html.png',
     'assets/img/sass.png',
     'assets/img/js.png',
@@ -19,5 +20,26 @@ export class SobreMiComponent {
     'assets/img/angular.png',
     'assets/img/php.png',
     'assets/img/laravel.png'
-  ]
+  ];
+
+  openPdfDialog(): void {
+    Swal.fire({
+      title: '¿Qué deseas hacer con el CV?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Ver',
+      denyButtonText: `Descargar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Navegar a la ruta del visor de PDF
+        window.open('/cv', '_blank');
+      } else if (result.isDenied) {
+        // Descargar el PDF
+        const link = document.createElement('a');
+        link.href = 'assets/files/cv.pdf';
+        link.download = 'CV-Sergio-Roman.pdf';
+        link.click();
+      }
+    });
+  }
 }
